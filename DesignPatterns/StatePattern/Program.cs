a409c840d52d885f9189f66998212923aae7ca35
nikolaiandrov <nikolaiandrov@abv.bg>
2025-03-31 17:07:50 +0300

    testing functionality

--- DesignPatterns/StatePattern/Program.cs
+++ DesignPatterns/StatePattern/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Document document = new Document(Enums.UserRoles.Editor);
+            Console.WriteLine(document.State);
+
+            document.Publish();
+            Console.WriteLine(document.State);
+
+            Document documentTwo = new Document(Enums.UserRoles.Admin);
+            Console.WriteLine(documentTwo.State);
+
+            documentTwo.Publish();
+            Console.WriteLine(documentTwo.State);
+
+            documentTwo.Publish();
+            Console.WriteLine(documentTwo.State);
         }
     }
 }

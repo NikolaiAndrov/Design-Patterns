49026ecea1a408e2c9442ad167a508b7bb83877a
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-01 14:11:41 +0300

    OverlayBlackAndWhite added

---
+++ DesignPatterns/StrategyPattern/OverlayBlackAndWhite.cs
@@ -0,0 +1,12 @@
+﻿namespace StrategyPattern
+{
+    using StrategyPattern.Contracts;
+
+    public class OverlayBlackAndWhite : IOverlay
+    {
+        public void Apply()
+        {
+            Console.WriteLine("Applying black and white overlay.");
+        }
+    }
+}

43a5e52ab672156aafe97552d626bb2d50589751
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-03 23:51:31 +0300

    testing functionality

--- DesignPatterns/IteratorPattern/Program.cs
+++ DesignPatterns/IteratorPattern/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace IteratorPattern
+﻿using IteratorPattern.Contracts;
+
+namespace IteratorPattern
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ShoppingList<string> list = new ShoppingList<string>();
+            list.Add("Banana");
+            list.Add("Melon");
+            list.Add("Sweets");
 
+            IIterator<string> iterator = list.GetIterator();
+
+            while (iterator.HasNext())
+            {
+                Console.WriteLine(iterator.Current());
+                iterator.Next();
+            }
+
+            list.Remove("Banana");
+            list.Add("Water");
+            iterator.Reset();
+
+            while (iterator.HasNext())
+            {
+                Console.WriteLine(iterator.Current());
+                iterator.Next();
+            }
+
+            ShoppingList<string> list2 = new ShoppingList<string>();
+            IIterator<string> iterator2 = list2.GetIterator();
+
+            while (iterator2.HasNext())
+            {
+                Console.WriteLine(iterator2.Current());
+                iterator2.Next();
+            }
         }
     }
 }

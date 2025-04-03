8193f609dcbff9601a3f2e7d3f7c8354466a981d
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-03 22:34:53 +0300

    shopping list implemented

---
+++ DesignPatterns/IteratorPattern/ShoppingList.cs
@@ -0,0 +1,22 @@
+﻿namespace IteratorPattern
+{
+    public class ShoppingList<T>
+    {
+        private readonly List<T> items;
+
+        public ShoppingList()
+        {
+            this.items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public bool Remove(T item)
+        {
+            return items.Remove(item);
+        }
+    }
+}

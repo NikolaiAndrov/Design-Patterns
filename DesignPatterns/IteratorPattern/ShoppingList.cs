0d3ed53ba83616a1c7b3d94ca1961b2639e31f8f
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-03 23:30:35 +0300

    listiterator added in shoppinglist

--- DesignPatterns/IteratorPattern/ShoppingList.cs
+++ DesignPatterns/IteratorPattern/ShoppingList.cs
@@ -1,5 +1,7 @@
 ﻿namespace IteratorPattern
 {
+    using IteratorPattern.Contracts;
+
     public class ShoppingList<T>
     {
         private readonly List<T> items;
@@ -17,6 +19,53 @@
         public bool Remove(T item)
         {
             return items.Remove(item);
+        }
+
+        public IIterator<T> GetIterator()
+        {
+            return new ListIterator(this);
+        }
+
+        private class ListIterator : IIterator<T>
+        {
+            private ShoppingList<T> list;
+            private int index;
+
+            public ListIterator(ShoppingList<T> list)
+            {
+                this.list = list;
+                this.index = 0;
+            }
+
+            public T Current()
+            {
+                if (this.index >= 0 && this.index < this.list.items.Count)
+                {
+                    return this.list.items[this.index];
+                }
+
+                throw new IndexOutOfRangeException("Invalid index!");
+            }
+
+            public bool HasNext()
+            {
+                if (this.index < this.list.items.Count - 1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Next()
+            {
+                this.index++;
+            }
+
+            public void Reset()
+            {
+                this.index = 0;
+            }
         }
     }
 }

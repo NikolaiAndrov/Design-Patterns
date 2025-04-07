bc140760d4183eb483a7ae8d6ca57a5ebbf4f219
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-07 16:59:50 +0300

    DimCommand implemented

---
+++ DesignPatterns/CommandPattern/DimCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace CommandPattern
+{
+    using Contracts;
+
+    class DimCommand : ICommand
+    {
+        private Light light;
+
+        public DimCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            this.light.Dim();
+        }
+    }
+}

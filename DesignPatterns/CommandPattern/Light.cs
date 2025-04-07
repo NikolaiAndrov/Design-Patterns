6651ccc45796dc73a0e412c6778490d90ad02943
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-07 16:49:42 +0300

    Light class implemented

---
+++ DesignPatterns/CommandPattern/Light.cs
@@ -0,0 +1,20 @@
+﻿namespace CommandPattern
+{
+    public class Light
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("The light is on!");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("The light is off!");
+        }
+
+        public void Dim()
+        {
+            Console.WriteLine("The light is dim!");
+        }
+    }
+}

385702e2f2d119ed263dede90629fd5b60d04db8
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-07 16:54:17 +0300

    TurnOnCommand implementd

---
+++ DesignPatterns/CommandPattern/TurnOnCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace CommandPattern
+{
+    using CommandPattern.Contracts;
+
+    public class TurnOnCommand : ICommand
+    {
+        private Light light;
+
+        public TurnOnCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            this.light.TurnOn();
+        }
+    }
+}

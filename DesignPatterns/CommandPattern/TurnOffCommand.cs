2e895332fdcea9576090505d8223748c9ed5ddca
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-07 16:57:36 +0300

    TurnOffCommand implemented

---
+++ DesignPatterns/CommandPattern/TurnOffCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace CommandPattern
+{
+    using Contracts;
+
+    public class TurnOffCommand : ICommand
+    {
+        private Light light;
+
+        public TurnOffCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            this.light.TurnOff();
+        }
+    }
+}

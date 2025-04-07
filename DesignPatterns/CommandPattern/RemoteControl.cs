7ad4021473a94448bbe224bdc854a603c2145b4e
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-07 16:44:54 +0300

    RemoteControl implemented

---
+++ DesignPatterns/CommandPattern/RemoteControl.cs
@@ -0,0 +1,24 @@
+﻿namespace CommandPattern
+{
+    using CommandPattern.Contracts;
+
+    public class RemoteControl
+    {
+        private ICommand command;
+
+        public RemoteControl(ICommand command)
+        {
+            this.command = command;
+        }
+
+        public void SetCommand(ICommand command)
+        {
+            this.command = command;
+        }
+
+        public void PressButton()
+        {
+            this.command.Execute();
+        }
+    }
+}

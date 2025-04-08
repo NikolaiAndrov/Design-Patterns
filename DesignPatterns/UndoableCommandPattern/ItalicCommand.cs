16c481bd3853997d10e24a72929364797bf8acd6
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-08 22:59:01 +0300

    ItalicCommand created execute implemented

---
+++ DesignPatterns/UndoableCommandPattern/ItalicCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace UndoableCommandPattern
+{
+    using UndoableCommandPattern.Contracts;
+
+    public class ItalicCommand : IUndoableCommand
+    {
+        private HtmlDocument document;
+        private History history;
+        private string prevContent = string.Empty;
+
+        public ItalicCommand(HtmlDocument document, History history)
+        {
+            this.document = document;
+            this.history = history;
+        }
+
+        public void Execute()
+        {
+            this.prevContent = document.Content;
+            this.document.MakeItalic();
+            this.history.Push(this);
+        }
+
+        public void Unexecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

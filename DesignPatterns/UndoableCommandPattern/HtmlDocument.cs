995ef9325260e9f2a2834fdf03a8b2f14e7a09bf
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-08 22:32:23 +0300

    HtmlDocument class added

---
+++ DesignPatterns/UndoableCommandPattern/HtmlDocument.cs
@@ -0,0 +1,16 @@
+﻿namespace UndoableCommandPattern
+{
+    public class HtmlDocument
+    {
+        public HtmlDocument()
+        {
+        }
+
+        public string Content { get; set; } = string.Empty;
+
+        public void MakeItalic()
+        {
+            this.Content = $"<i>{this.Content}</i>";
+        }
+    }
+}

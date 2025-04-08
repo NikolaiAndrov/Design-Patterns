ed4dcfcf849a2d98226c90033a0312cf929a099e
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-08 22:25:44 +0300

    undoable command pattern initial create

---
+++ DesignPatterns/UndoableCommandPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UndoableCommandPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

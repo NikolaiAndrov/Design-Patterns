a1855d0c0039863c1f908584ac84e09a80cd52da
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-08 23:12:35 +0300

    undo command implemented

---
+++ DesignPatterns/UndoableCommandPattern/UndoCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace UndoableCommandPattern
+{
+    using UndoableCommandPattern.Contracts;
+
+    internal class UndoCommand : ICommand
+    {
+        private History history;
+
+        public UndoCommand(History history) 
+        {
+            this.history = history;
+        }
+
+        public void Execute()
+        {
+            if (this.history.Size() > 0)
+            {
+                IUndoableCommand lastCommand = this.history.Pop();
+                lastCommand.Unexecute();
+            }
+        }
+    }
+}

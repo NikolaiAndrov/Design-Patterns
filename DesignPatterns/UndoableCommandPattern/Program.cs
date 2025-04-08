6f0f4b1da739eec945c75825f3a7365a7a34f43b
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-08 23:21:19 +0300

    testing functionality

--- DesignPatterns/UndoableCommandPattern/Program.cs
+++ DesignPatterns/UndoableCommandPattern/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            HtmlDocument doc = new HtmlDocument();
+            History history = new History();
+            doc.Content = "Hello World!";
+            Console.WriteLine(doc.Content);
+
+            ItalicCommand italicCommand = new ItalicCommand(doc, history);
+            italicCommand.Execute();
+            Console.WriteLine(doc.Content);
+
+            UndoCommand undoCommand = new UndoCommand(history);
+            undoCommand.Execute();
+            Console.WriteLine(doc.Content);
         }
     }
 }

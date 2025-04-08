da1cc94dfe1730ccaba014deddfa1bd7c9207a62
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-08 22:46:28 +0300

    history class added

---
+++ DesignPatterns/UndoableCommandPattern/History.cs
@@ -0,0 +1,34 @@
+﻿namespace UndoableCommandPattern
+{
+    using Contracts;
+
+    public class History
+    {
+        private Stack<IUndoableCommand> commands;
+
+        public History()
+        {
+            this.commands = new Stack<IUndoableCommand>();
+        }
+
+        public void Push(IUndoableCommand command)
+        {
+            this.commands.Push(command);
+        }
+
+        public IUndoableCommand Pop()
+        {
+            if (this.commands.Count == 0)
+            {
+                throw new InvalidOperationException("Command history empty!");
+            }
+
+            return this.commands.Pop(); 
+        }
+
+        public int Size()
+        {
+            return this.commands.Count;
+        }
+    }
+}

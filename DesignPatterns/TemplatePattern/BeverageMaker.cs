7ab614111ff452cca71bfd491b8544ee8e15511a
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-09 17:07:56 +0300

    BeverageMaker class and functionality added

---
+++ DesignPatterns/TemplatePattern/BeverageMaker.cs
@@ -0,0 +1,36 @@
+﻿namespace TemplatePattern
+{
+    using Contracts;
+
+    public class BeverageMaker
+    {
+        private IBeverage beverage;
+
+        public BeverageMaker(IBeverage beverage)
+        {
+            this.beverage = beverage;
+        }
+
+        public void SetBeverage(IBeverage beverage)
+        {
+            this.beverage = beverage;
+        }
+
+        public void MakeBeverage()
+        {
+            this.BoilWater();
+            this.PourInCup();
+            this.beverage.Prepare();
+        }
+
+        private void BoilWater()
+        {
+            Console.WriteLine("Boiling water.");
+        }
+
+        private void PourInCup()
+        {
+            Console.WriteLine("Pouring water into cup.");
+        }
+    }
+}

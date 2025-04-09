6b91617db959fa086e170a2b6811036016d64007
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-09 16:49:25 +0300

    tea clas with functionality added

---
+++ DesignPatterns/TemplatePattern/Tea.cs
@@ -0,0 +1,33 @@
+﻿namespace TemplatePattern
+{
+    using Contracts;
+
+    public class Tea : IBeverage
+    {
+        public void Prepare()
+        {
+            this.Brew();
+            this.AddCondiments();
+        }
+
+        private void Brew()
+        {
+            Console.WriteLine("Brewing tea for 3 minutes.");
+        }
+
+        private void AddCondiments()
+        {
+            if (AreCondimentsNeeded())
+            {
+                Console.WriteLine("Adding lemon to the tea.");
+            }
+        }
+
+        private bool AreCondimentsNeeded()
+        {
+            Console.WriteLine("Would you like lemon? (y/n)");
+            string input = Console.ReadLine()!;
+            return input.ToLower() == "y";
+        }
+    }
+}

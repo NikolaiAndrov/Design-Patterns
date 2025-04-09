2c5f80a304105e5740b69dd3ab39e7955ed64e5e
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-09 16:58:53 +0300

    Camomile class added

---
+++ DesignPatterns/TemplatePattern/Camomile.cs
@@ -0,0 +1,17 @@
+﻿namespace TemplatePattern
+{
+    using Contracts;
+
+    public class Camomile : IBeverage
+    {
+        public void Prepare()
+        {
+            this.Brew();
+        }
+
+        private void Brew()
+        {
+            Console.WriteLine("Brewing camomile for 3 minutes.");
+        }
+    }
+}

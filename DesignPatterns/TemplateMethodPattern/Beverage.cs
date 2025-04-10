c91fc13ccf4fecf31df67623c97bc65252b31d3c
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-10 22:06:53 +0300

    Beverage abstract class for template created

---
+++ DesignPatterns/TemplateMethodPattern/Beverage.cs
@@ -0,0 +1,28 @@
+﻿namespace TemplateMethodPattern
+{
+    public abstract class Beverage
+    {
+        public void Prepare()
+        {
+            this.BoilWater();
+            this.PourInCup();
+        }
+
+        private void BoilWater()
+        {
+            Console.WriteLine("Boiling water.");
+        }
+
+        private void PourInCup()
+        {
+            Console.WriteLine("Pouring water into cup.");
+        }
+
+        protected abstract void Brew();
+
+        protected virtual void AddCondiments()
+        {
+
+        }
+    }
+}

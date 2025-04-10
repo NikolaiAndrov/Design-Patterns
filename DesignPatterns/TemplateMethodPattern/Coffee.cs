0353cf424eb7fa462f018282afd8abf991666a3f
nikolaiandrov <nikolaiandrov@abv.bg>
2025-04-10 22:17:49 +0300

    coffee class created

---
+++ DesignPatterns/TemplateMethodPattern/Coffee.cs
@@ -0,0 +1,25 @@
+﻿namespace TemplateMethodPattern
+{
+    public class Coffee : Beverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Brewing coffee for 3 minutes.");
+        }
+
+        protected override void AddCondiments()
+        {
+            if (AreCondimentsNeeded())
+            {
+                Console.WriteLine("Adding milk to the coffee.");
+            }
+        }
+
+        private bool AreCondimentsNeeded()
+        {
+            Console.WriteLine("Would you like some milk? (y/n)");
+            string input = Console.ReadLine()!;
+            return input.ToLower() == "y";
+        }
+    }
+}

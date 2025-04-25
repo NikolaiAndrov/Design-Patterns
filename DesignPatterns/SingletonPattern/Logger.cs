b8b12356c7ef3dc62c01e544cc435c7f8137966d
Nikolay Androv <nandrov@kpmgadvisory.be>
2025-04-25 14:41:51 +0300

    logger class created

---
+++ DesignPatterns/SingletonPattern/Logger.cs
@@ -0,0 +1,43 @@
+﻿namespace SingletonPattern
+{
+    public sealed class Logger
+    {
+        private static Logger? instance;
+        private static readonly object padlock = new();
+        private List<string> logs = new();
+
+        private Logger()
+        {
+            
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)      
+                        {
+                            instance = new Logger();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            this.logs.Add($"Date: {DateTime.Now} Message: {message}");
+        }
+
+        public string GetLogs()
+        {
+            return string.Join(Environment.NewLine, this.logs);
+        }
+    }
+}

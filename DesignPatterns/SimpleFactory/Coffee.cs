f2727b3c9b1c8317624abc245e7568623c62afae
nikolaiandrov <nikolaiandrov@abv.bg>
2025-05-06 22:47:27 +0300

    coffee class added

---
+++ DesignPatterns/SimpleFactory/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleFactory
+{
+    public class Coffee
+    {
+        public Coffee(int water, int milk)
+        {
+            this.Water = water;
+            this.Milk = milk;
+        }
+
+        public int Water { get; set; }
+
+        public int Milk { get; set; }
+    }
+}

b504130f0fffd5ec4a2433d46d3644cda5bf1d82
nikolaiandrov <nikolaiandrov@abv.bg>
2025-05-11 21:38:05 +0300

    IPhone class added

---
+++ DesignPatterns/FactoryMethodPattern/Products/IPhone.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryMethodPattern.Products
+{
+    public class IPhone : Gsm
+    {
+        public IPhone()
+        {
+            this.Name = "IPhone";
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Booting up IPhone");
+            Console.WriteLine("Welcome to your IPhone");
+        }
+    }
+}

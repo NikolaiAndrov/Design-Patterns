4081b67dc44e018e621d4282ca316fce67dda2d5
nikolaiandrov <nikolaiandrov@abv.bg>
2025-05-11 21:29:00 +0300

    Gsm abstract class created

---
+++ DesignPatterns/FactoryMethodPattern/Products/Gsm.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace FactoryMethodPattern.Products
+{
+    public abstract class Gsm
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int BatteryCapacity { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.Name}");
+            sb.AppendLine($"BatteryCapacity: {this.BatteryCapacity}");
+            sb.AppendLine($"Weight: {this.Weight}");
+            sb.AppendLine($"Height: {this.Height}");
+            sb.AppendLine($"Width: {this.Width}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

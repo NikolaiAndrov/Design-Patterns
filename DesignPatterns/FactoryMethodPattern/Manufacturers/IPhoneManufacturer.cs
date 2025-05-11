1993ff0bfc7e2411de0b239dcad37ceda1207e0b
nikolaiandrov <nikolaiandrov@abv.bg>
2025-05-11 21:54:38 +0300

    IPhoneManufacturer class added

---
+++ DesignPatterns/FactoryMethodPattern/Manufacturers/IPhoneManufacturer.cs
@@ -0,0 +1,18 @@
+﻿namespace FactoryMethodPattern.Manufacturers
+{
+    using FactoryMethodPattern.Products;
+
+    public class IPhoneManufacturer : Manufacturer
+    {
+        public override Gsm ManufactureGsm()
+        {
+            return new IPhone()
+            {
+                BatteryCapacity = 5000,
+                Weight = 200,
+                Height = 150,
+                Width = 80
+            };
+        }
+    }
+}

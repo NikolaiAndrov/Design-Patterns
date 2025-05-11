54d4fa2cbc173a542186583580fec99dd0750c67
nikolaiandrov <nikolaiandrov@abv.bg>
2025-05-11 21:44:36 +0300

    SamsungGalaxy class added

---
+++ DesignPatterns/FactoryMethodPattern/Products/SamsungGalaxy.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryMethodPattern.Products
+{
+    public class SamsungGalaxy : Gsm
+    {
+        public SamsungGalaxy()
+        {
+            this.Name = "Samsung Galaxy";   
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Booting up Samsung Galaxy");
+            Console.WriteLine("Welcome to your Samsung Galaxy");
+        }
+    }
+}

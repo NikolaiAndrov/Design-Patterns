d0aea81ad38343b13db22a3be1400727696e3d0c
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-16 15:12:33 +0300

    TextBox added

---
+++ DesignPatterns/MediatorPattern/TextBox.cs
@@ -0,0 +1,25 @@
+﻿namespace MediatorPattern
+{
+    public class TextBox : UIControl
+    {
+        private string text = string.Empty;
+
+        public TextBox(DialogBox owner) : base(owner)
+        {
+        }
+
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+
+            set
+            {
+                this.text = value;
+                this.owner.Changed(this);
+            }
+        }
+    }
+}

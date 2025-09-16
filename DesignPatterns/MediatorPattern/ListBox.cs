18ff06e89d8e805aad53ab05583fc61e3a986f62
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-16 15:07:47 +0300

    ListBox added

---
+++ DesignPatterns/MediatorPattern/ListBox.cs
@@ -0,0 +1,25 @@
+﻿namespace MediatorPattern
+{
+    public class ListBox : UIControl
+    {
+        private string selection = string.Empty;
+
+        public ListBox(DialogBox owner) : base(owner)
+        {
+        }
+
+        public string Selection
+        {
+            get 
+            {
+                return this.selection; 
+            }
+
+            set
+            {
+                this.selection = value;
+                this.owner.Changed(this);
+            }
+        }
+    }
+}

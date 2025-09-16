3099757cdade7a8462603362ddd5790b0b4eb963
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-16 15:49:52 +0300

    PostDialogBox created - concrete mediator

---
+++ DesignPatterns/MediatorPattern/Button.cs
@@ -0,0 +1,25 @@
+﻿namespace MediatorPattern
+{
+    public class Button : UIControl
+    {
+        private bool isEnabled = false;
+
+        public Button(DialogBox owner) : base(owner)
+        {
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+
+            set
+            {
+                this.isEnabled = value;
+                this.owner.Changed(this);
+            }
+        }
+    }
+}
---
+++ DesignPatterns/MediatorPattern/PostsDialogBox.cs
@@ -0,0 +1,40 @@
+﻿namespace MediatorPattern
+{
+    public class PostsDialogBox : DialogBox
+    {
+        private Button button;
+        private TextBox textBox;
+        private ListBox listBox;
+
+        public PostsDialogBox()
+        {
+            this.button = new Button(this);
+            this.textBox = new TextBox(this);
+            this.listBox = new ListBox(this);
+        }
+
+        public override void Changed(UIControl uIControl)
+        {
+            if (uIControl == this.listBox)
+            {
+                this.HandlePostChanged();
+            }
+            else if (uIControl == this.textBox)
+            {
+                this.HandleTitleChanged();
+            }
+        }
+
+        private void HandlePostChanged()
+        {
+            this.textBox.Text = this.listBox.Selection;
+            this.button.IsEnabled = true;
+        }
+
+        private void HandleTitleChanged()
+        {
+            bool isTitleEmpty = this.textBox.Text == "";
+            this.button.IsEnabled = !isTitleEmpty;
+        }
+    }
+}
